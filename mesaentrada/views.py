735d93d7888ec6cfaede15a217f176326c1dd98f
habprof2017 <habprof2017@gmail.com>
2017-05-21 10:35:49 -0300

    agregada tabla q muestra todas las notas

--- mesaentrada/views.py
+++ mesaentrada/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+# from .forms import NotaModelForm
+from .models import Nota
 
 
 def inicio(request):
-    return render(request, 'mesaentrada/mesaentrada_index.html', {})
+    todaslasNotas = Nota.objects.all()
+    titulo = 'Hola desconocido'
+    vacio = '-'
+    if request.user.is_authenticated():
+        titulo = 'Hola %s' % (request.user)
+    # form = NotaModelForm(request.POST or None)
+    context = {
+        "notas": todaslasNotas,
+        "un_titulo": titulo,
+        "vacio": vacio,
+    }
+
+    # if form.is_valid():
+    #   instance = form.save(commit=False)
+    #  	instance.save()
+
+    return render(request, 'mesaentrada/mesaentrada_index.html', context)
+
+# poner el nombre del usuario arriba
+# cambiar el contexto
+# mostar toda la lista ?
